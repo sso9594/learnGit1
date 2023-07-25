b2e78d3d6750254564420c8178b9c62b04f91554
sso9594 <sso9594@naver.com>
2023-07-25 14:05:25 +0900

    add function print_git

--- hello.py
+++ hello.py
@@ -1,6 +1,13 @@
+import sys
+
 def pring_message():
 	message = "world"
 	print("hello{}".format(message))
 
+def pring_git():
+	message = "from the hell"
+	sys.print("git {}".format(message))
+
 if __name__ == "__main__":
-	print_message() 
+	print_message()
+	print_git()  
